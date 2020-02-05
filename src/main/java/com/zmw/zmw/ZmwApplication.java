2258cd6f497af1678b49b57942d5f8d929e31962
zjc <=>
2020-02-05 21:47:58 +0800

    init repo

---
+++ src/main/java/com/zmw/zmw/ZmwApplication.java
@@ -0,0 +1,13 @@
+package com.zmw.zmw;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZmwApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ZmwApplication.class, args);
+    }
+
+}
---
+++ src/main/java/com/zmw/zmw/controller/HelloController.java
@@ -0,0 +1,20 @@
+package com.zmw.zmw.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller  //允许这个类接收前端的请求
+public class HelloController {
+
+
+        @GetMapping("/hello")
+        public String hello(@RequestParam(name = "name") String name, Model model)
+        {
+            model.addAttribute("name",name);
+            return "hello";
+        }
+
+
+}
