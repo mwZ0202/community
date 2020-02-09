41186bcd68f05f116b8318f888e748533e87345f
mwZ0202 <1772960999@qq.com>
2020-02-09 16:32:01 +0800

    add index and bootstrap

--- src/main/java/com/zmw/zmw/controller/IndexController.java
+++ src/main/java/com/zmw/zmw/controller/IndexController.java
@@ -6,15 +6,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller  //允许这个类接收前端的请求
-public class HelloController {
-
-
-        @GetMapping("/hello")
-        public String hello(@RequestParam(name = "name") String name, Model model)
-        {
-            model.addAttribute("name",name);
-            return "hello";
-        }
-
-
+public class IndexController {
+        @GetMapping("/")
+        public String index(){ return "index"; }
 }
