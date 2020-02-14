3073f4a32eb17a2e32168777ffd62ac6b5205616
mwZ0202 <1772960999@qq.com>
2020-02-14 14:49:31 +0800

    add sql ddl

--- README.md
+++ README.md
@@ -10,6 +10,13 @@
 [Github OAuth Document](https://developer.github.com/apps/building-oauth-apps/creating-an-oauth-app/)
 [OkHttp](https://square.github.io/)
 [Mysql](https://www.runoob.com/mysql/mysql-tutorial.html)
+[H2](http://www.h2database.com/html/main.html)
+[Spring](https://docs.spring.io/spring-boot/docs/2.0.0.RC1/reference/htmlsingle/#boot-features-embedded-database-support)
 
+
+##工具
 [Git](https://git-scm.com/)
 [Visual Paradigm](https://www.visual-paradigm.com)
+
+
+##脚本
--- src/main/java/com/zmw/zmw/controller/AuthorizeController.java
+++ src/main/java/com/zmw/zmw/controller/AuthorizeController.java
@@ -3,6 +3,8 @@ package com.zmw.zmw.controller;
 
 import com.zmw.zmw.dto.AccessTokenDTO;
 import com.zmw.zmw.dto.GithubUser;
+import com.zmw.zmw.mapper.UserMapper;
+import com.zmw.zmw.model.User;
 import com.zmw.zmw.provider.GithubProvider;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
@@ -12,6 +14,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 
 import javax.servlet.http.HttpServletRequest;
 import java.io.IOException;
+import java.util.UUID;
 
 @Controller
 public class AuthorizeController {
@@ -29,6 +32,9 @@ public class AuthorizeController {
     @Value("${github.redirect.uri}")
     private String redirectUri;
 
+    @Autowired
+    private UserMapper userMapper;
+
 
     @GetMapping("/callback")
     public String callback(@RequestParam(name="code") String code,
@@ -41,11 +47,18 @@ public class AuthorizeController {
         accessTokenDTO.setRedirect_uri(redirectUri);
         accessTokenDTO.setState(state);
         String accessToken = githubProvider.getAccessToken(accessTokenDTO);
-        GithubUser user = githubProvider.getUser(accessToken);
-        System.out.println(user.getName());
-        if(user !=null)
+        GithubUser githubUser = githubProvider.getUser(accessToken);
+        System.out.println(githubUser.getName());
+        if(githubUser !=null)
         {
-            request.getSession().setAttribute("user",user);
+            User user = new User();
+            user.setToken(UUID.randomUUID().toString());
+            user.setName(githubUser.getName());
+            user.setAccountId(String.valueOf(githubUser.getId()));
+            user.setGmtCreate(System.currentTimeMillis());
+            user.setGmtModified(user.getGmtCreate());
+            userMapper.insert(user);
+            request.getSession().setAttribute("user",githubUser);
             return "redirect:/"; //重定向到index
             //登陆成功，写cookie 和 session session从request中拿到
         }
